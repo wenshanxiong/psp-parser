6655d21ecce4a72ff6545850b9ec38d88faec27c
Wenshan Xiong <wenshan.xiong@yahooinc.com>
2023-11-02 22:23:30 -0400

    added option to parse tomorrow's prices

--- psp_parser.py
+++ psp_parser.py
@@ -1,10 +1,12 @@
+import argparse
+import sqlite3
+import logging
+import queries
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import sqlite3
-import logging
 from sqlite3 import OperationalError
-import queries
+from time import sleep
 
 PSP_URL = 'https://www.ameren.com/illinois/account/customer-service/bill/power-smart-pricing/prices'
 PSP_DB = 'psp.db'
@@ -28,14 +30,19 @@ def init_db():
         logging.info('Table already exists, skipped creation')
     return con
 
-def parse_psp(con):
+def parse_psp(con, parse_tomorrow):
     price_map = {}
 
     options = Options()
     options.add_argument('headless')
     driver = webdriver.Chrome(options=options)
-
     driver.get(PSP_URL)
+
+    if parse_tomorrow:
+        tomorrow_btn = driver.find_element(By.XPATH, '//*[@id="rtp-tomorrow"]')
+        tomorrow_btn.click()
+        sleep(5)
+
     table = driver.find_element(By.XPATH, '/html/body/div[2]/ameren-manage-program/ng-component/section/div/div/div[3]/div/div[4]/div/table/tbody')
     rows = table.find_elements(By.TAG_NAME, 'tr')
 
@@ -47,6 +54,7 @@ def parse_psp(con):
         price_cent = rows[h].find_elements(By.TAG_NAME, 'td')[1].text[:-1]
         price_map[h] = price_cent
     try:
+        print(price_map)
         cur = con.cursor()
         cur.execute(queries.get_insert_query(price_map))
         con.commit()
@@ -57,8 +65,13 @@ def parse_psp(con):
     return price_map
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--tomorrow", help="Parse tomorrow's prices",
+                        action="store_true")
+    args = parser.parse_args()
+
     con = init_db()
-    parse_psp(con)
+    parse_psp(con, args.tomorrow)
     con.close()
 
 if __name__ == "__main__":
